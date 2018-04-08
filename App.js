3a029cab1d2d60dedc77464a550ce6f9ab701bcc
John Fansler <john.fansler@gmail.com>
2018-04-07 20:34:37 -0700

    Image rotation changed at random on press

--- App.js
+++ App.js
@@ -10,13 +10,18 @@ export default class App extends React.Component {
         }
     }
 
+    rotateBottle(){
+        let newDeg = Math.random() * 360;
+        this.setState({bottleRotation: newDeg})
+    }
+
     render() {
         let {width} = Dimensions.get('window');
         width = width * .9;
         let bottleRotation = this.state.bottleRotation.toString() + 'deg';
         return (
             <View style={styles.container}>
-                <TouchableOpacity onPress={()=>{alert('pressed')}}>
+                <TouchableOpacity onPress={()=>{this.rotateBottle()}}>
                     <Image style={{width, height: width, transform:[{ rotate: bottleRotation }] }} resizeMode={'contain'} source={require('./img/bottle.jpg')}/>
                 </TouchableOpacity>
             </View>
